747d734bba0fd788d0495bdcf4ad6089101a6e5a
Danny Hua <danny.hua@gmail.com>
2020-11-01 10:42:52 -0800

    strip .html from posts too

--- _plugins/no-html.rb
+++ _plugins/no-html.rb
@@ -1,9 +1,18 @@
 require 'fileutils'
 
-# Remove all .html ext of posts, for pretty url and S3 restriction
+# Remove all .html ext of pages and posts, for pretty url and S3 restriction
 # Production only
 
-Jekyll::Hooks.register :pages, :post_write do |post|
+Jekyll::Hooks.register :pages, :post_write do |page|
+  if Jekyll.env == 'production'
+    path = page.destination('/')
+    if path.end_with?('.html')
+      FileUtils.mv(path, path.sub(/\.html$/, ''))
+    end
+  end
+end
+
+Jekyll::Hooks.register :posts, :post_write do |post|
   if Jekyll.env == 'production'
     path = post.destination('/')
     if path.end_with?('.html')
